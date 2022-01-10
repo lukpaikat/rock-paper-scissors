9a4da8b52592afc968eb1b242040a77769e3b76e
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-10 17:12:13 +0800

    add game logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,33 @@
 console.log("Hello World");
+
+const randomGenerator = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+const computerPlay = () => {
+  // randomly return rock, paper, or scissors
+  const choices = {
+    1: "rock",
+    2: "paper",
+    3: "scissors"
+  }
+  const randomNumber = randomGenerator(1, 3);
+  return choices[randomNumber];
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  // return a string that declares the winner of the round
+  if (playerSelection === computerSelection) {
+    return "Draw!";
+  }
+
+  const strongAgainstRules = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
+  if (strongAgainstRules[playerSelection][computerSelection]) {
+    return "You win!"
+  } else {return "You lose!"}
+};

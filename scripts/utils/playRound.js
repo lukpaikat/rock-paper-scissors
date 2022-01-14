d5a2a94ca86f0fad4f0c0f15cc9770aed8f9b7ce
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-14 08:14:54 +0800

    created playRound.js

---
+++ scripts/utils/playRound.js
@@ -0,0 +1,16 @@
+const whoIsWeakAgainst = require('../data/whoIsWeakAgainst');
+
+const playRound = (playerSelection, computerSelection) => {
+  // return a string that declares the winner of the round
+  console.log(`You pick ${playerSelection} and the computer pick ${computerSelection}`)
+  if (playerSelection === computerSelection) {
+    return "Draw!";
+  }
+  if (whoIsWeakAgainst(playerSelection) === computerSelection) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+};
+
+module.exports = playRound;

059c1fbbd13f8b9a7470266278949202d3b21126
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-14 08:16:51 +0800

    created btn-initiator.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,1 @@
-const computerPlay = require('./utils/computerPlay.js')
-
-// what the code should do
-// TODO: click btn to play the game
-// TODO: keeps score
-// eslint-disable-next-line no-unused-vars
-const initButton = (btnId, btnStrInput) => {
-  // give event listeners to the game buttons
-  const btnElement = document.getElementById(btnId);
-  const roundResultElement = document.getElementById('round-result');
-  btnElement.addEventListener('click', () => {
-    roundResultElement.textContent = playRound(btnStrInput, computerPlay());
-  });
-}
+// execute initial functions here
---
+++ scripts/utils/btn-initiator.js
@@ -0,0 +1,14 @@
+const computerPlay = require('./utils/computerPlay.js');
+const playRound = require('../utils/playRound');
+// what the code should do
+// TODO: click btn to play the game
+// TODO: keeps score
+// eslint-disable-next-line no-unused-vars
+const initButton = (btnId, btnStrInput) => {
+  // give event listeners to the game buttons
+  const btnElement = document.getElementById(btnId);
+  const roundResultElement = document.getElementById('round-result');
+  btnElement.addEventListener('click', () => {
+    roundResultElement.textContent = playRound(btnStrInput, computerPlay());
+  });
+}

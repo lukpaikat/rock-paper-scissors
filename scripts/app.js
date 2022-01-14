74a1d7b044bddc705a7fd8659b539a0da234e529
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-14 08:14:40 +0800

    created playRound.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,5 @@
 const computerPlay = require('./utils/computerPlay.js')
 
-const playRound = (playerSelection, computerSelection) => {
-  // return a string that declares the winner of the round
-  console.log(`You pick ${playerSelection} and the computer pick ${computerSelection}`)
-  if (playerSelection === computerSelection) {
-    return "Draw!";
-  }
-  if (whoIsWeakAgainst(playerSelection) === computerSelection) {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
-  }
-};
-
 // what the code should do
 // TODO: click btn to play the game
 // TODO: keeps score

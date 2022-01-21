e92113f78ca49461cd70beb3e8af526930735e1f
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-21 19:37:38 +0800

    write test openingThePage without importing app()

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const main = document.querySelector('main');
   main.innerHTML = /* html */ `
-  <h1 class="game-title">Rock Paper Scissors</h1>
-  <p class="game-sub-title">The game</p>
+    <h1 class="game-title">Rock Paper Scissors</h1>
+    <p class="game-sub-title">The game</p>
 
-  <button id="start-button">Start game</button>
-`
+    <button id="start-button">Start game</button>
+  `
 });
--- specs/openingThePage.spec.js
+++ specs/openingThePage.spec.js
@@ -9,6 +9,7 @@ document.body.innerHTML = /*html*/ `
 // TODO: create a function that generate welcome page in main tag
 describe('opening the game page', () => {
   test('the browser should display a welcome page', () => {
+    // coba taruh app() di sini
     const main = document.querySelector('main');
     main.innerHTML = /* html */ `
       <h1 class="game-title">Rock Paper Scissors</h1>

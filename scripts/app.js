3ebd5cd1168b6c64057b60831010ff36f54559e9
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-21 14:33:40 +0800

    added what the code should do

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const initButton = require('./utils/btn-initiator');
 
 // execute initial functions here
+// this code should make browser display welcome page
 document.addEventListener('DOMContentLoaded', () => {
   const rockBtn = document.getElementById('rock-btn');
   const paperBtn = document.getElementById('paper-btn');

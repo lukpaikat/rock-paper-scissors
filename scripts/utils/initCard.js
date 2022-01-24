ff14755c280919085a7d14a8a3b37014e574d2c8
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-24 09:57:22 +0800

    rename button initiator to initCard

--- scripts/utils/initCard.js
+++ scripts/utils/initCard.js
@@ -5,7 +5,7 @@ const computerPlay = require('./utils/computerPlay.js');
 const playRound = require('../utils/playRound');
 // what the code should do
 // TODO: click btn to pick the game choice
-const initButton = (btnElement, btnStrInput) => {
+const initCard = (btnElement, btnStrInput) => {
   // give event listeners to the game buttons
   const roundResultElement = document.getElementById('round-result');
   btnElement.addEventListener('click', () => {
@@ -13,4 +13,4 @@ const initButton = (btnElement, btnStrInput) => {
   });
 }
 
-module.exports = initButton;
+module.exports = initCard;

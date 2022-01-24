23253b3cd80183693892a68bcab61057be970a24
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-24 13:32:16 +0800

    remove scoreUpdater

--- scripts/utils/scoreUpdater.js
+++
@@ -1,7 +0,0 @@
-// TODO: update score to game data
-const scoreUpdater = (scoreElm, score) => {
-  const prevScore = parseInt(scoreElm.textContent);
-  scoreElm.textContent = prevScore + score;
-};
-
-module.exports = scoreUpdater;

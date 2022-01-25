69d550a7a27a731fb0868d20fca2de3ee87847c9
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-25 15:50:44 +0800

    add comment on RoundPickPage.render()

--- scripts/views/showdownPage.js
+++ scripts/views/showdownPage.js
@@ -19,12 +19,13 @@ const ShowdownPage = {
     </div>
     <button id="continue-button">continue</button>
     `;
+    this.afterRender();
   },
 
   afterRender() {
     const continueButton = document.getElementById('continue-button');
     continueButton.addEventListener('click', () => {
-      RoundPickPage.render();
+      RoundPickPage.render(); // sementara
       // this function show continue button
       // continue button contains scoreChecking function
       // which wether return the player to round pick or game result

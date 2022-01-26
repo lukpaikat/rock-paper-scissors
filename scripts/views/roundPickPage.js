6fb055447291917de6e8dafc090135006f94d1f2
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-26 13:29:21 +0800

    add span to each number of score count text

--- scripts/views/roundPickPage.js
+++ scripts/views/roundPickPage.js
@@ -7,16 +7,19 @@ const RoundPickPage = {
     const { round, player, computer } = runningScore;
     const main = document.querySelector('main');
     main.innerHTML = /* html */ `
-      <h2 class="round-count">Round: ${round}</h2>
-      <div class="running-score-container">
-        <p>Player: ${player}</p>
-        <p>Computer: ${computer}</p>
+      <div class="round-pick">
+        <h2 class="round-heading">Round: <span id=round-count>${round}</span></h2>
+        <div class="running-score-container">
+          <p>Player: <span id="player-score">${player}</span></p>
+          <p>Computer: <span id="computer-score">${computer}</span></p>
+        </div>
+        <p>First to 5 win</p>
+        <p>Pick a card to play</p>
+        <button id="rock-button">Rock</button>
+        <button id="paper-button">Paper</button>
+        <button id="scissors-button">Scissors</button>
       </div>
-      <p>First to 5 win</p>
-      <p>Pick a card to play</p>
-      <button id="rock-button">Rock</button>
-      <button id="paper-button">Paper</button>
-      <button id="scissors-button">Scissors</button>
+      <div class="overlay"></div>
     `;
     this.afterRender();
   },

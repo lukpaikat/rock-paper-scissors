42784d8c1328d35e073ac839c04a8cdd42f14c98
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-27 02:16:32 +0800

    create round pick page display setter

---
+++ scripts/utils/roundPickPageDisplay.js
@@ -0,0 +1,6 @@
+const roundPickPageDisplay = (attribute) => {
+  const roundPickPage = document.querySelector('.round-pick');
+  roundPickPage.style.display = attribute;
+};
+
+export default roundPickPageDisplay;

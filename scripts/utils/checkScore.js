8f589c00dbc2974025cfa29d9a94f767fdb351ed
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-26 08:15:58 +0800

    fix dependecy cycle to welocome page

--- scripts/utils/checkScore.js
+++ scripts/utils/checkScore.js
@@ -1,16 +1,15 @@
 import runningScore from '../data/runningScore.js';
 import RoundPickPage from '../views/roundPickPage.js';
-import WelcomePage from '../views/welcomePage.js';
 
 const checkScore = () => {
   const { player, computer } = runningScore;
   if (player >= 5) {
     window.alert(`You win the game! ${player}-${computer}`);
-    WelcomePage.render();
+    window.location.reload();
   }
   if (computer >= 5) {
     window.alert(`You lose the game! & ${player}-${computer}`);
-    WelcomePage.render();
+    window.location.reload();
   }
   if (computer < 5 && player < 5) {
     RoundPickPage.render();

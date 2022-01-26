5e8230cb4b8d41eb486575ddec825b76cb180e41
lukpaikat <mhasbisaputra5@gmail.com>
2022-01-27 02:16:10 +0800

    apply final result page to check score

--- scripts/utils/checkScore.js
+++ scripts/utils/checkScore.js
@@ -1,17 +1,28 @@
 import runningScore from '../data/runningScore.js';
 import updateScore from './updateScore.js';
+import FinalResultPage from '../views/finalResultPage.js';
 
 const checkScore = () => {
   const { player, computer } = runningScore;
   if (player >= 5) {
     // TODO: display final result
-    window.alert(`You win the game! ${player}-${computer}`);
-    window.location.reload();
+    FinalResultPage.render({
+      message: 'You win the game!',
+      playerScore: player,
+      computerScore: computer,
+    });
+    // window.alert(`You win the game! ${player}-${computer}`);
+    // window.location.reload();
   }
   if (computer >= 5) {
     // TODO: display final result
-    window.alert(`You lose the game! & ${player}-${computer}`);
-    window.location.reload();
+    FinalResultPage.render({
+      message: 'You lose the game!',
+      playerScore: player,
+      computerScore: computer,
+    });
+    // window.alert(`You lose the game! & ${player}-${computer}`);
+    // window.location.reload();
   }
   if (computer < 5 && player < 5) {
     const overlay = document.querySelector('.overlay');
